5ff53f1b3604e16129bdab2c37607ef2a98a63d1
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-10-30 00:04:38 +0200

    Setting up the main project ...

---
+++ src/main/java/com/cleverdeveloper/Main.java
@@ -0,0 +1,12 @@
+package com.cleverdeveloper;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 10/30/2022 12:03 AM
+*/
+
+public class Main {
+    /* Write code here ... #### */
+}
