090b11d87e5c0061c2d43c20c75763cd0a01b5df
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-10-30 00:49:52 +0200

    DRY principle [example two] from : huongdanjava

---
+++ src/main/java/com/cleverdeveloper/dry/example_two/Calculator.java
@@ -0,0 +1,24 @@
+package com.cleverdeveloper.dry.example_two;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 10/30/2022 12:45 AM
+*/
+
+public class Calculator {
+
+    public int total(int a, int b) {
+        int sum = a + b;
+        System.out.println("Total = " + sum);
+        return sum;
+    }
+
+    public double average(int a, int b) {
+        int average = total(a,b) / 2;
+        System.out.println("Average = " + average);
+        return average;
+    }
+
+}
