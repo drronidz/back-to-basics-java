c9ff88c54a1575cb655d683995061442de74e42f
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-10-31 00:38:10 +0100

    IDMI principle [example one] from : baeldung

---
+++ src/main/java/com/cleverdeveloper/idmi/example_one/Vehicle.java
@@ -0,0 +1,30 @@
+package com.cleverdeveloper.idmi.example_one;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 10/31/2022 12:06 AM
+*/
+
+public interface Vehicle {
+
+    String getBrand();
+    String speedUp();
+    String slowDown();
+
+    double getSpeed();
+
+    public default String turnAlarmOn() {
+        return "Turning the vehicle alarm on.";
+    }
+
+    public default String turnAlarmOff() {
+        return "Turning the vehicle alarm off.";
+    }
+
+    public default double getSpeedInKMH(double speed) {
+        return speed * 1.60934;
+    }
+
+}
