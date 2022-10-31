ced6ca5ecece1cd60c715ca700b21b1e390fe08e
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-10-31 01:06:58 +0100

    Favor Composition over Inheritance principle [example one] from : gfg ...

---
+++ src/main/java/com/cleverdeveloper/fcoij/example_one/inheritance/Person.java
@@ -0,0 +1,15 @@
+package com.cleverdeveloper.fcoij.example_one.inheritance;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 10/31/2022 12:54 AM
+*/
+
+public class Person {
+    public int id;
+    public String position;
+    public String name;
+    public int age;
+}
