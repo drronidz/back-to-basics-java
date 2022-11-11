3fbbdbf209de40bd1f597c3b841ea5f211706bdb
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-11-11 15:49:26 +0100

    Singleton Pattern [Creational Pattern] : Lazy

---
+++ src/main/java/com/cleverdeveloper/gof/creational/singleton/lazy/LazySingleton.java
@@ -0,0 +1,40 @@
+package com.cleverdeveloper.gof.creational.singleton.lazy;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 11/11/2022 3:34 PM
+*/
+
+public class LazySingleton {
+    private static volatile LazySingleton lazy_single_instance = null;
+
+    private LazySingleton() {
+
+    }
+
+    // without double-checked-locking principle
+    public static LazySingleton getInstanceWithoutDoubleCheckLocking() {
+        if (lazy_single_instance == null) {
+            synchronized (LazySingleton.class) {
+                lazy_single_instance = new LazySingleton();
+            }
+        }
+        return lazy_single_instance;
+    }
+
+    // with double-checked-locking-principle
+    public static LazySingleton getInstanceWithDoubleCheckLocking() {
+        if (lazy_single_instance == null) {
+            synchronized (LazySingleton.class) {
+                if (lazy_single_instance == null) {
+                    lazy_single_instance = new LazySingleton();
+                }
+            }
+        }
+        return  lazy_single_instance;
+    }
+
+
+}
