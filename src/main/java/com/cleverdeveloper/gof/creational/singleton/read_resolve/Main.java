1d20876156ec167e1b784738b648d87b7236314c
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-11-11 21:57:40 +0100

    Singleton Pattern [Creational Pattern] : readResolve()

---
+++ src/main/java/com/cleverdeveloper/gof/creational/singleton/read_resolve/Main.java
@@ -0,0 +1,35 @@
+package com.cleverdeveloper.gof.creational.singleton.read_resolve;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 11/11/2022 4:29 PM
+*/
+
+import java.io.*;
+
+public class Main {
+    static Singleton instanceOne = Singleton.getInstance();
+
+    public static void main(String[] args) {
+        try {
+            // Serialize to a file
+            ObjectOutput output = new ObjectOutputStream(new FileOutputStream("read_resolve.ser"));
+            output.writeObject(instanceOne);
+            output.close();
+
+            instanceOne.setI(20);
+
+            // Serialize to a file
+            ObjectInput input = new ObjectInputStream(new FileInputStream("read_resolve.ser"));
+            Singleton instanceTwo = (Singleton) input.readObject();
+            input.close();
+
+            System.out.println(instanceOne.getI());
+            System.out.println(instanceTwo.getI());
+        } catch (IOException | ClassNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/cleverdeveloper/gof/creational/singleton/read_resolve/Singleton.java
@@ -0,0 +1,37 @@
+package com.cleverdeveloper.gof.creational.singleton.read_resolve;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 11/11/2022 4:26 PM
+*/
+
+import java.io.Serializable;
+
+public class Singleton implements Serializable {
+
+    private volatile static Singleton single_instance = null;
+
+    public static Singleton getInstance() {
+        return single_instance == null
+                ? single_instance = new Singleton()
+                : single_instance;
+    }
+
+    // To avoid multiple instances creation
+    protected  Object readResolve() {
+        return single_instance;
+    }
+
+    private int i = 10;
+
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
