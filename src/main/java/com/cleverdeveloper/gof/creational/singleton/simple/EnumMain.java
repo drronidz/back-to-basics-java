ce0415700d5ea13cb047ce163e45d4a0f26c1b3c
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-11-11 16:20:37 +0100

    Singleton Pattern [Creational Pattern] : Enum

---
+++ src/main/java/com/cleverdeveloper/gof/creational/singleton/simple/EnumMain.java
@@ -0,0 +1,18 @@
+package com.cleverdeveloper.gof.creational.singleton.simple;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 11/11/2022 4:18 PM
+*/
+
+public class EnumMain {
+    public static void main(String[] args) {
+        EnumSingleton enumSingleton = EnumSingleton.SINGLE_INSTANCE;
+
+        System.out.println(enumSingleton.getValue());
+        enumSingleton.setValue(15);
+        System.out.println(enumSingleton.getValue());
+    }
+}
---
+++ src/main/java/com/cleverdeveloper/gof/creational/singleton/simple/EnumSingleton.java
@@ -0,0 +1,21 @@
+package com.cleverdeveloper.gof.creational.singleton.simple;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 11/11/2022 4:16 PM
+*/
+
+public enum  EnumSingleton {
+    SINGLE_INSTANCE;
+    int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
