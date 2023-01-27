84421ef5d46e5d097a3b6b61ad2262c39a2e504b
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2023-01-27 16:05:46 +0100

    Functional Interfaces [Predicate.negate()] : [codegym].

---
+++ src/main/java/com/cleverdeveloper/functional_interfaces/PredicateNegateDriver.java
@@ -0,0 +1,21 @@
+package com.cleverdeveloper.functional_interfaces;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 1/27/2023 4:05 PM
+*/
+
+import java.util.function.Predicate;
+
+public class PredicateNegateDriver {
+    public static void main(String[] args) {
+
+        Predicate<Integer> adult = age -> age >= 18;
+
+        System.out.println(adult.negate().test(7));
+        System.out.println(adult.negate().test(19));
+
+    }
+}
