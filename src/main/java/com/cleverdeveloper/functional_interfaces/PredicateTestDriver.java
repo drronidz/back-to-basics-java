ecde1e784ed22ebd45cd819430140354876cdb01
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2023-01-27 15:59:19 +0100

    Functional Interfaces [Predicate.test()] : [codegym].

---
+++ src/main/java/com/cleverdeveloper/functional_interfaces/PredicateTestDriver.java
@@ -0,0 +1,22 @@
+package com.cleverdeveloper.functional_interfaces;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 1/27/2023 3:58 PM
+*/
+
+import java.util.function.Predicate;
+
+public class PredicateTestDriver {
+
+    public static void main(String[] args) {
+
+        Predicate<Integer> adult = age -> age >= 18;
+        System.out.println(adult.test(12));
+        System.out.println(adult.test(19));
+        System.out.println(adult.test(21));
+
+    }
+}
