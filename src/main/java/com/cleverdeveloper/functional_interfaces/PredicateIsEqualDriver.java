e0686350b62724de6bb6a8567d8ef15776fc2296
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2023-01-27 16:06:58 +0100

    Functional Interfaces [Predicate.isEqual(T arg)] : [codegym].

---
+++ src/main/java/com/cleverdeveloper/functional_interfaces/PredicateIsEqualDriver.java
@@ -0,0 +1,20 @@
+package com.cleverdeveloper.functional_interfaces;
+
+/*
+PROJECT NAME : Back 2 Basics (Java Core)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 1/27/2023 4:06 PM
+*/
+
+import java.util.function.Predicate;
+
+public class PredicateIsEqualDriver {
+    public static void main(String[] args) {
+        // Static method
+        Predicate<String> i = Predicate.isEqual("here is my crayon");
+        System.out.println(i.test("here is my pencil"));
+        System.out.println(i.test("here is my crayon"));
+
+    }
+}
